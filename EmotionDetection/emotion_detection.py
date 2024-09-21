24928a4be148de7720c621a05b7abe6e5b20f559
nilsriga <nils.riga@gmail.com>
2024-09-21 10:29:14 -0400

    finished exercises

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,24 +3,40 @@ import json
 
 def emotion_detector(text_to_analyze):
 
-    response = requests.post(
-        'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict',
-        headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
-        json =  { "raw_document": { "text": text_to_analyze } }
-        )
+    try:
 
-    emotion_scores = json.loads(response.text)["emotionPredictions"][0]["emotion"]
+        if text_to_analyze and len(text_to_analyze) > 0:
 
-    dominant_emotion = sorted(emotion_scores.items(), key=lambda item: item[1], reverse=True)[0][0]
+            response = requests.post(
+                'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict',
+                headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
+                json =  { "raw_document": { "text": text_to_analyze } }
+                )
 
-    emotion_scores |= {"dominant_emotion": dominant_emotion}
+            emotion_scores = json.loads(response.text)["emotionPredictions"][0]["emotion"]
 
-    desired_order = ["anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"]
-    
-    sorted_emotions = {key: emotion_scores[key] for key in desired_order if key in emotion_scores}
+            dominant_emotion = sorted(emotion_scores.items(), key=lambda item: item[1], reverse=True)[0][0]
 
-    json_response = json.dumps(sorted_emotions, indent=4, separators=(',', ': '))
+            emotion_scores |= {"dominant_emotion": dominant_emotion}
 
-    return json_response
+            desired_order = ["anger", "disgust", "fear", "joy", "sadness", "dominant_emotion"]
+            
+            sorted_emotions = {key: emotion_scores[key] for key in desired_order if key in emotion_scores}
 
-print(emotion_detector("I love this new technology"))
+            json_response = json.dumps(sorted_emotions, indent=4, separators=(',', ': '))
+
+            return json_response
+
+        else:
+
+            return {
+                    "anger": None, 
+                    "disgust": None, 
+                    "fear": None, 
+                    "joy": None, 
+                    "sadness": None, 
+                    "dominant_emotion": None
+                    }
+    except:
+            return {"message": "invalid input"}, 404
+# print(emotion_detector("I love this new technology"))
--- server.py
+++ server.py
@@ -1,18 +1,36 @@
-from flask import Flask, request, render_template, jsonify
+# pylint: disable=E0401
+"""
+This imports the API library flask and the Watson AI analysis custom library
+"""
+from flask import Flask, request, render_template
 from EmotionDetection import emotion_detection
-from pprint import pprint
-import json
 
 app = Flask(__name__)
 
+"""
+This module creates a Flask web application for emotion detection.
+It renders an index page and analyzes text for emotions using a GET request.
+"""
+
 @app.route('/')
 def index():
+    """
+    Renders the index page.
+    """
     return render_template('index.html')
 
 
 @app.route("/emotionDetector", methods=['GET'])
 def analyze_text():
-    
+    """
+    Analyzes the provided text and detects emotions.
+    Returns a message if the text is invalid.
+    """
     text_to_analyze = request.args.get("textToAnalyze")
 
-    return emotion_detection.emotion_detector(text_to_analyze), 200
+    analyzed_text = emotion_detection.emotion_detector(text_to_analyze)
+
+    if analyzed_text["dominant_emotion"] is None:
+        return {"message": "Invalid text! Please try again!"}, 200
+
+    return analyzed_text, 200
